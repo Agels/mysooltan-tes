1d2b30926585ad79fc8ace77ddb409a16dd92f83
Agels <ardhianagilr@gmail.com>
2022-06-28 09:33:36 +0700

    make it better

--- src/app/reducer/combine.js
+++ src/app/reducer/combine.js
@@ -3,5 +3,4 @@ import { repoReducer } from './repoReducer';
 
 export const reducer = combineReducers({
     repo:repoReducer,
-    token:"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"
 })
--- src/component/avatar/index.jsx
+++ src/component/avatar/index.jsx
@@ -1,27 +1,26 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {avatar} from '../../app/actions/repoAction';
+import { avatar } from "../../app/actions/repoAction";
 const Avatar = () => {
-    const dispatch = useDispatch();
-    const token = useSelector((state) => state.repo.token);
-    const avatars = useSelector(state => state.repo.avatar);
-    useEffect(() => {
-        axios
-          .get("https://api.github.com/users/Agels", 
-          {
-            headers: {
-              authorization: `token ${token}`
-            }
-          })
-          .then((res) =>dispatch(avatar(res.data)));
-      }, []);
-    return (
-        <>
-        <img src={avatars.avatar_url} className="rounded-circle w-75" alt="img" />
-        <p className="fs-4">{avatars.login}</p>
-        </>
-    )
-}
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.repo.token);
+  const avatars = useSelector((state) => state.repo.avatar);
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/Agels", {
+        headers: {
+          authorization: `token ${token}`,
+        },
+      })
+      .then((res) => dispatch(avatar(res.data)));
+  }, []);
+  return (
+    <>
+      <img src={avatars.avatar_url} className="rounded-circle w-75" alt="img" />
+      <p className="fs-4">{avatars.login}</p>
+    </>
+  );
+};
 
 export default Avatar;
