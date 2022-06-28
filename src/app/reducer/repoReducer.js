6bf43e36de0467700da3d7c31aad97df595d284f
Agels <ardhianagilr@gmail.com>
2022-06-28 10:20:45 +0700

    setting token

--- src/app/reducer/repoReducer.js
+++ src/app/reducer/repoReducer.js
@@ -3,7 +3,7 @@ import {AVATAR, REPO} from '../constant/constant';
 const initialState = {
     repo:[],
     avatar:{},
-    token:"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"
+    token:"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"
 }
 
 export const repoReducer = (state = initialState,{type,payload}) => {
